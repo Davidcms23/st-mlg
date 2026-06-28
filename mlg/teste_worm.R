20f1fa6f8899881ef40895d83c16fed5f5c1b975
Davidcms23 <martinschristiandavid@gmail.com>
2026-06-28 13:54:08 -0300

    Travado no cloglog

--- mlg/teste_worm.R
+++ mlg/teste_worm.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(MASS)
+library(car)
 library(faraway)
 library(drc)
 
-# O ensaio contamina um comartimento do solo com diferentes níveis de substância nociva e observa quantas minhocas resistem e permanecem no local, em oposoção às que migram para o solo vizinho limpo.
+# O ensaio contamina um compartimento do solo com diferentes níveis de substância nociva e observa quantas minhocas resistem e permanecem no local, em oposição às que migram para o solo vizinho limpo.
 
 data(earthworms)
 
@@ -15,5 +16,12 @@ mod_cloglog <- glm(cbind(number, total - number) ~ dose,
                    family = binomial(link = "cloglog"), data = earthworms)
 
 AIC(mod_logit, mod_probit, mod_cloglog) # critério para escolher o melhor modelo
+# cloglog tem o menor AIC. Portanto, Cloglog é o vencedor 
 
+dev_res <- deviance(mod_cloglog)
+gl_res <- df.residual(mod_cloglog)
+
+p_valor <- 1 - pchisq(dev_res, gl_res)
+
+tibble(Desvio=dev_res, gl = gl_res, "p-valor" = p_valor)
 
