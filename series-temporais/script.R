efa4bdf724106a5352a48390761ef45dac8d6b82
Davidcms23 <martinschristiandavid@gmail.com>
2026-07-02 16:23:22 -0300

    Nova serie

--- series-temporais/script.R
+++ series-temporais/script.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(fpp3)
-dados <- read.csv("~/Downloads/Dados_Estatisticos.csv", sep = ";")
+dados <- read.table("dados/Dados_Estatisticos.csv", sep = ";", header = TRUE, fill = TRUE)
+
 dados <- dados |> select(ANO, MES, AEROPORTO_DE_ORIGEM_PAIS) |> 
   filter(AEROPORTO_DE_ORIGEM_PAIS == "BRASIL") |> 
   mutate(ANOMES = paste0(ANO, "/", MES)) |> 
