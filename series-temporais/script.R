ffbe88213de86ba26d4da5da30be58df9da28aa7
Davidcms23 <martinschristiandavid@gmail.com>
2026-07-03 20:45:38 -0300

    Transformando e ajustando o modelo

--- series-temporais/script.R
+++ series-temporais/script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(fpp3)
+library(fpp2)
 library(tsibble)
 library(dplyr)
 library(urca)
@@ -12,7 +13,7 @@ dados <- read_delim(
   skip = 1
 )
 
-voos_serie <- dados |> 
+voos <- dados |> 
   select(ANO, MES, AEROPORTO_DE_ORIGEM_PAIS) |>  
   filter(AEROPORTO_DE_ORIGEM_PAIS == "BRASIL") |>  
   mutate(ANOMES = paste0(ANO, "/", MES)) |>  
@@ -21,35 +22,46 @@ voos_serie <- dados |>
   count() |> 
   as_tsibble(index = ANOMES)
 
-voos <- ts(voos$n, frequency = 12)
+# voos |> autoplot(log(n) |>  difference(12))
 
-ndiffs(voos)  # diferenças simples necessárias
-nsdiffs(voos) # diferenças sazonais necessárias
+voos20 <- voos |> 
+  filter(ANOMES < yearmonth("2020 jan"))
+  
+autoplot(voos20)
 
-autoplot(voos)
+ndiffs(ts(voos20))
+nsdiffs(ts(voos20, frequency = 12))
+
+voos20 |> autoplot(log(n) |> difference(12))
 
 # Testar estacionaridade
-summary(ur.kpss(voos))
-adf.test(voos)
+summary(
+  ur.kpss(
+    diff(log(voos20$n), lag = 12)
+  )
+)
+adf.test(
+  diff(log(voos20$n), lag = 12)
+)
 
-voos_serie |> gg_tsdisplay(n, plot_type = 'partial')
+voos20 |> gg_tsdisplay(
+  log(n) |> difference(12),
+  plot_type = 'partial',
+  lag = 36
+  )
+# Pela analise da ACF e PACF temos um SARIMA(2, 0, 2)(2, 1, 2)
 
-acf(voos)
-pacf(voos)
+auto.arima(voos) # (1, 1, 2)(2, 0, 0)
 
-arima(voos, order = c(1,0,0)) 
-arima(voos, order = c(2,0,0))
-arima(voos, order = c(1,0,1))
-arima(voos, order = c(2,0,1))
+fit <- voos20 |> 
+  model(
+    best2 = ARIMA(log(n) ~ 0 + pdq(2, 0, 2) + PDQ(2, 1, 2,)),
+    best = ARIMA(log(n) ~ 0 + pdq(2, 0, 1) + PDQ(2, 1, 2,)),
+    auto = ARIMA(log(n))
+  )
 
-fit1 <- Arima(voos, order = c(2, 0, 1))
-autoplot(fit1)
+report(fit)
+accuracy(fit)
 
-fit2 <- Arima(voos, order = c(1, 0, 0))
-autoplot(fit2) 
 
-checkresiduals(fit2)
 
-fit2 |> forecast(h = 10) |> autoplot()
-
-auto.arima(voos)
