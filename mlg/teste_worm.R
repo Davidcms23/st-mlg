61736f8589a55a68dd74059617a8b13440f2136a
Davidcms23 <martinschristiandavid@gmail.com>
2026-07-04 15:32:53 -0300

    Finalndo as sessões até a parte descritiva

--- mlg/teste_worm.R
+++ mlg/teste_worm.R
@@ -7,20 +7,46 @@ library(drc)
 # O ensaio contamina um compartimento do solo com diferentes níveis de substância nociva e observa quantas minhocas resistem e permanecem no local, em oposição às que migram para o solo vizinho limpo.
 
 data(earthworms)
+str(earthworms) 
+summary(earthworms) 
 
-mod_logit <- glm(cbind(number, total - number) ~ dose,
-                 family = binomial(link = "logit"), data = earthworms)
-mod_probit <- glm(cbind(number, total - number) ~ dose,
-                  family = binomial(link = "probit"), data = earthworms)
-mod_cloglog <- glm(cbind(number, total - number) ~ dose,
-                   family = binomial(link = "cloglog"), data = earthworms)
+head(earthworms)
 
-AIC(mod_logit, mod_probit, mod_cloglog) # critério para escolher o melhor modelo
-# cloglog tem o menor AIC. Portanto, Cloglog é o vencedor 
+dados_plot <- earthworms %>%
+  mutate(proporcao = number / total)
 
-dev_res <- deviance(mod_cloglog)
-gl_res <- df.residual(mod_cloglog)
+ggplot(dados_plot, aes(x = dose, y = proporcao)) +
+  geom_point(color = "darkblue", size = 3, alpha = 0.7) +
+  theme_minimal() +
+  labs(
+    itle = "Proporção de Minhocas Retidas vs Dose da Substância Tóxica",
+    x = "Dose",
+    y = "Proporção Empírica (Ficaram/Total)"
+  ) +
+  geom_hline(yintercept = 0.5, linetype = "dashed", color = "red")
 
-p_valor <- 1 - pchisq(dev_res, gl_res)
+earthworms |> 
+  filter(dose > 0) |> 
+  ggplot(aes(x = log(dose), y = number/total)) +
+  geom_point(color = "darkblue", size = 3, alpha = 0.7) +
+  geom_smooth(method = "loess", se = FALSE, color = "red", linetype = "dashed") + 
+  theme_minimal() +
+  labs(
+    title = "Proporção de Retenção vs Logaritmo da Dose Ativa",
+    x = "Log(Dose) (Excluindo o Controle)",
+    y = "Proporção Empírica"
+  )
 
-tibble(Desvio=dev_res, gl = gl_res, "p-valor" = p_valor)
+earthworms |> 
+  filter(dose > 0) |> 
+  # logito empírico: log((sucessos + 0.5) / (fracassos + 0.5))
+  mutate(elogit = log((number + 0.5) / (total - number + 0.5))) %>%
+  ggplot(aes(x = log(dose), y = elogit)) +
+  geom_point(color = "darkblue", size = 3, alpha = 0.7) +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  theme_minimal() +
+  labs(
+    title = "Logitos Empíricos vs Log(Dose)",
+    x = "Log(Dose) (Excluindo o Controle)",
+    y = "Logito Empírico"
+  )
