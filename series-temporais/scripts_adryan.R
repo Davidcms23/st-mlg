538a8a4e8d3ff7426e02e2463685430040598109
alligatordbigod <adryanextreme9@gmail.com>
2026-07-05 16:47:37 -0300

    Estacionaridade

--- series-temporais/scripts_adryan.R
+++ series-temporais/scripts_adryan.R
@@ -1,5 +1,5 @@
 rm(list = ls()); gc(full = TRUE)
-library(tidyverse); library(fpp3); library(fpp2)
+library(tidyverse); library(fpp3); library(fpp2); library(urca); library(tseries)
 
 dados <- readRDS("dados/Dados_Estatisticos.rds")
 #write_csv(dados, "~/Downloads/Dados_Estatisticos2.csv")
@@ -8,6 +8,9 @@ dados <- dados |>
   dplyr::ungroup()
 dados |> autoplot()
 dados |> ACF(lag_max = 42) |> autoplot()
+
+dados |> gg_season()
+dados |> gg_subseries()
 
 dados |> 
   features(n, features = guerrero) # sem transformação
@@ -65,3 +68,47 @@ ajustes_reduzidos |>
 ajustes_reduzidos |> 
   augment() |> 
   features(.innov, ljung_box, lag = 24, dof = 3) # dof varia conforme os parâmetros do modelo
+
+nsdiffs(ts(dados, frequency = 12))
+ndiffs(ts(dados))
+
+dados |> autoplot(n |> difference(1)) # Bem melhor
+dados |> autoplot(n |> difference(6))
+dados |> autoplot(n |> difference(12))
+dados |> autoplot(difference(difference(n, 1), 12))
+dados |> autoplot(difference(difference(n, 1), 6))
+
+dados |> 
+  features(n, list(unitroot_kpss, unitroot_nsdiffs, unitroot_ndiffs))
+
+dados |> 
+  features(n |> difference(1), list(unitroot_kpss, unitroot_nsdiffs, unitroot_ndiffs))
+
+dados |> 
+  features(n |> difference(1) |> difference(12), list(unitroot_kpss, unitroot_nsdiffs, unitroot_ndiffs))
+
+
+adf.test(
+  diff(diff(dados$n, lag = 12), lag = 1) # Passa
+)
+
+summary(
+  ur.kpss(
+    diff(diff(dados$n, lag = 12), lag = 1) # Passa
+  )
+)
+
+adf.test(
+  diff(dados$n, lag = 1) # Passa
+)
+
+adf.test(
+  diff(dados$n, lag = 12) # Não passa
+)
+
+dados |> 
+  gg_tsdisplay(
+    difference(n, 1) |> difference(12), 
+    plot_type = "partial",
+    lag_max = 26
+  )
